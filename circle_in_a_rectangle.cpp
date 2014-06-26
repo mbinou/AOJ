63ac53ed7bd1e77e4d7766996b80679448b58959
OniB <onib.muu@gmail.com>
2014-06-26 14:54:41 +0900

    Create circle_in_a_rectangle.cpp

---
+++ circle_in_a_rectangle.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int main () {
+  int W, H, x, y, r;
+  cin >> W >> H >> x >> y >> r;
+
+  if (x>=r && x<=W-r && y>=r && y<=H-r) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+}
